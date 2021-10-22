e35edfa417721b5e1de129789d8f232e6baa7edf
Christopher Ling <cling1@mail.sfsu.edu>
2021-10-22 09:02:01 -0700

    some tests for starting out

--- fsInit.c
+++ fsInit.c
@@ -25,10 +25,53 @@
 #include "mfs.h"
 
 
+typedef struct volumeControlBlock{
+		char VCBsignature[100];
+		int numberOfBlocks;
+		int blockSize;
+		int location;
+		int rootStartLocation;
+		int freeSpaceStartLocation;
+}volumeControlBlock;
+		
+typedef struct directoryEntry{
+	int size;
+	int location;
+	char name[100];
+}directoryEntry;
+
+typedef struct freeSpace{ // might not need this struct, maybe just a helper function to init entry to free to be true
+	unsigned free:1;
+}freeSpace;
+
+
+
 int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	{
 	printf ("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 	/* TODO: Add any code you need to initialize your file system. */
+
+	//assuming VCB hasn't been made yet
+	char testSignature[100] = "101";
+
+	// Initialize VCB
+	printf("initializing VCB");
+	volumeControlBlock VCB;
+	memcpy(VCB.VCBsignature, testSignature, strlen(testSignature));
+	printf("VCB's signature is %s\n", VCB.VCBsignature);
+	VCB.numberOfBlocks = numberOfBlocks;
+	VCB.blockSize = blockSize;
+	VCB.location = 0; // Always starts at location 0
+	VCB.rootStartLocation = 1;  // letting root start after VCB block spanning 10 blocks
+	VCB.freeSpaceStartLocation = 10; // free space will start after root's block
+	
+	freeSpace freeSpaceTracker[numberOfBlocks];
+	for(int i=0; i< VCB.freeSpaceStartLocation; i++){ // setting 0 for not free
+		freeSpaceTracker[i].free = 0;
+	}
+	for(int i = VCB.freeSpaceStartLocation; i < numberOfBlocks; i++){ // setting 1 for free
+		freeSpaceTracker[i].free = 1;
+	}
 
 	return 0;
 	}
